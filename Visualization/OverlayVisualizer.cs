2c4daed70e07a231d1e85892fdaabafd3eb81a77
searica <143636061+searica@users.noreply.github.com>
2023-12-26 10:27:51 -0800

    Formatting and Bool optimizations

--- Visualization/OverlayVisualizer.cs
+++ Visualization/OverlayVisualizer.cs
@@ -1,11 +1,9 @@
 ﻿using HarmonyLib;
 using UnityEngine;
 
-namespace TerrainTools.Visualization
-{
+namespace TerrainTools.Visualization {
     // Helper classes for OverlayVisualizerImpls, intended to abstract away necessary low level complexity.
-    public abstract class OverlayVisualizer : MonoBehaviour
-    {
+    public abstract class OverlayVisualizer : MonoBehaviour {
         protected Overlay primary;
         protected Overlay secondary;
         protected Overlay tertiary;
@@ -13,10 +11,8 @@ namespace TerrainTools.Visualization
 
         internal static readonly Vector3 VerticalOffset = new(0, 0.075f, 0);
 
-        private void Update()
-        {
-            if (primary == null)
-            {
+        private void Update() {
+            if (primary == null) {
                 var primaryTransform = transform.Find("_GhostOnly");
                 var secondaryTransform = Instantiate(primaryTransform, transform);
                 var tetriaryTransform = Instantiate(secondaryTransform, transform);
@@ -39,8 +35,7 @@ namespace TerrainTools.Visualization
 
         protected abstract void OnRefresh();
 
-        protected void SpeedUp(Overlay overlay)
-        {
+        protected void SpeedUp(Overlay overlay) {
             var animationCurve = new AnimationCurve();
             animationCurve.AddKey(0.0f, 0.0f);
             animationCurve.AddKey(0.5f, 1.0f);
@@ -50,53 +45,44 @@ namespace TerrainTools.Visualization
             overlay.SizeOverLifetime = minMaxCurve;
         }
 
-        protected void Freeze(Overlay overlay)
-        {
+        protected void Freeze(Overlay overlay) {
             overlay.StartSpeed = 0;
             overlay.SizeOverLifetimeEnabled = false;
         }
 
-        protected void VisualizeTerraformingBounds(Overlay overlay)
-        {
+        protected void VisualizeTerraformingBounds(Overlay overlay) {
             overlay.StartSize = 3.0f;
             overlay.psr.material.mainTexture = IconCache.Box;
             overlay.LocalPosition = VerticalOffset;
         }
 
-        protected void VisualizeIconInsideTerraformingBounds(Overlay overlay, Texture iconTexture)
-        {
+        protected void VisualizeIconInsideTerraformingBounds(Overlay overlay, Texture iconTexture) {
             overlay.StartSize = 2.5f;
             overlay.psr.material.mainTexture = iconTexture;
             overlay.LocalPosition = VerticalOffset;
         }
 
-        protected void VisualizeRecoloringBounds(Overlay overlay)
-        {
+        protected void VisualizeRecoloringBounds(Overlay overlay) {
             overlay.StartSize = 4.0f;
             overlay.psr.material.mainTexture = IconCache.Box;
             overlay.LocalPosition = VerticalOffset;
         }
 
-        protected void VisualizeIconInsideRecoloringBounds(Overlay overlay, Texture iconTexture)
-        {
+        protected void VisualizeIconInsideRecoloringBounds(Overlay overlay, Texture iconTexture) {
             overlay.StartSize = 3.0f;
             overlay.psr.material.mainTexture = iconTexture;
             overlay.Position = transform.position + VerticalOffset;
         }
     }
 
-    public abstract class HoverInfoEnabled : OverlayVisualizer
-    {
-        protected override void Initialize()
-        {
+    public abstract class HoverInfoEnabled : OverlayVisualizer {
+        protected override void Initialize() {
             hoverInfo.Color = secondary.StartColor;
         }
 
-        protected override void OnRefresh()
-        {
+        protected override void OnRefresh() {
             hoverInfo.Enabled = TerrainTools.IsHoverInforEnabled;
-            if (hoverInfo.Enabled)
-            {
+            if (hoverInfo.Enabled) {
                 hoverInfo.RotateToPlayer();
                 var pos = secondary.Position - VerticalOffset;
                 hoverInfo.Text = $"x: {pos.x:0}, y: {pos.y:0.000}, z: {pos.z:0}";
@@ -108,11 +94,9 @@ namespace TerrainTools.Visualization
     ///     Blocks check for invalid placement height if custom overlay
     /// </summary>
     [HarmonyPatch(typeof(Piece), "SetInvalidPlacementHeightlight")]
-    public static class OverlayVisualizationRedshiftHeigthlightBlocker
-    {
-        private static bool Prefix(Piece __instance)
-        {
-            return __instance.GetComponentInChildren<OverlayVisualizer>() == null;
+    public static class OverlayVisualizationRedshiftHeigthlightBlocker {
+        private static bool Prefix(Piece __instance) {
+            return !__instance.GetComponentInChildren<OverlayVisualizer>();
         }
     }
 }
