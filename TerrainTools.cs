71849e4f7b7727c5e7d614cd22e1fc0be7311435
searica <143636061+searica@users.noreply.github.com>
2024-12-07 09:29:51 -0800

    Update: bump version

--- TerrainTools.cs
+++ TerrainTools.cs
@@ -24,7 +24,7 @@ namespace TerrainTools {
         internal const string Author = "Searica";
         public const string PluginName = "AdvancedTerrainModifiers";
         public const string PluginGUID = $"{Author}.Valheim.TerrainTools";
-        public const string PluginVersion = "1.4.0";
+        public const string PluginVersion = "1.4.1";
 
         #region Section Names
 
